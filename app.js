5d201f921cf7e8ed369a70b972e5d29c554ac55a
Gabriel Elias Faudoa <Gabrielfaudoa@icloud.com>
2019-01-03 16:11:55 -0700

    Connected to Database on MLab

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const port = 4000
 const parser = require('body-parser')
+const cors = require('cors')
+const monk = require('monk')
+const url = 'mongodb://admin:password1@ds153113.mlab.com:53113/myguys'
+const db = monk(url)
+db.then(() => {
+  console.log('Connected correctly to the server')
+})
+
+const people = db.get('myguys')
+const port = 3030
 
 app.use(parser.json());
 app.use(cors());
 
 // GET method route
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage')
+app.get('/', async function (req, res) {
+    const results = await people.find ({})
+    res.status(200).send(results)
   })
   
   // POST method route
   app.post('/', function (req, res) {
+    const result = people.insert(req.body)
     res.status(200).send(req.body)
   })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
